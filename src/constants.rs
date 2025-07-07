c7b6cc280ba0f0e0c444b49ed6d3285b2c7091ef
beber89 <mahmoudbaibars@gmail.com>
2025-07-07 12:48:39 +0300

    Squashed 'prereq-rs/' content from commit 4d8c7cd
    git-subtree-dir: prereq-rs
    git-subtree-split: 4d8c7cd8660980ce9d70b562e78f6fd46723aab6

---
+++ src/constants.rs
@@ -0,0 +1,11 @@
+
+pub const RPC_URL: &str = "https://turbine-solanad-4cde.devnet.rpcpool.com/9a9da9cf-6db1-47dc-839a-55aca5c9c80a";
+pub const MY_TURBIN3_PUB: &str = "vitobZ3VjMwrZAJ6k2W7krx4HVhArrp1eQZLdJgVoN6";
+
+pub const TURBIN3_PROGRAM: &str = "TRBZyQHB3m68FGeVsqTK39Wm4xejadjVhP5MAZaKWDM";
+
+pub const COLLECTION: &str = "5ebsp5RChCGK7ssRZMVMufgVZhd2kFbNaotcZ5UvytN2";
+pub const MPL_CORE: &str = "CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d";
+
+
+
