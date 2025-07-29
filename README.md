fbbf3871a6b38ff85aa74e5487ba4a6186bcede7
beber89 <mahmoudbaibars@gmail.com>
2025-07-29 12:17:15 +0300

    Wired phoenix project with JS hooks to connect to solana devnet

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+## Notes [Temp]
+
+- Run postgres docker container
+```
+docker run --name postgres_db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
+```
+
--- phoenix_dapp/lib/phoenix_dapp_web/router.ex
+++ phoenix_dapp/lib/phoenix_dapp_web/router.ex
@@ -18,6 +18,8 @@ defmodule PhoenixDappWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    # Inside the scope "/" block with LiveDemoWeb as the pipeline
+    live "/counter", CounterLive, :index
   end
 
   # Other scopes may use custom stacks.
