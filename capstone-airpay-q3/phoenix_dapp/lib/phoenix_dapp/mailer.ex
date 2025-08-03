0a3e2d4f41a56a529e95548c556ece205757617d
beber89 <mahmoudbaibars@gmail.com>
2025-08-04 00:21:20 +0300

    FIX: git issue with prefix capstone-airpay-q3 subtree - had to fix by removing the subtree and add new one

--- capstone-airpay-q3/phoenix_dapp/lib/phoenix_dapp/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule PhoenixDapp.Mailer do
-  use Swoosh.Mailer, otp_app: :phoenix_dapp
-end
--- capstone-airpay-q3/phoenix_dapp/lib/phoenix_dapp/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule PhoenixDapp.Repo do
-  use Ecto.Repo,
-    otp_app: :phoenix_dapp,
-    adapter: Ecto.Adapters.Postgres
-end
